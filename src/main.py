ad0867c074d0e530a2b1d6563809dc82d8f4b570
alroa <alroal98@gmail.com>
2022-09-21 17:53:14 +0200

    Config adjustment

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 import argparse
 from data.create_datasets import create_datasets
 from models.pipeline import model_pipeline
+import json
+from PATHS import CONFIG_JSON_PATH
 from config import CFG
+
 
 if __name__ == "__main__":
 
@@ -13,118 +16,121 @@ if __name__ == "__main__":
         "--data_path",
         type=str,
         help="Specify a diferent data path from the default 'path/to/hubmap-organ-segmentation/data'",
-        default=CFG.data_path
+        default=CFG["data_path"]
     )
 
     parser.add_argument(
         "--num_class",
         type=int,
         help="Number of different classes to segment",
-        default=CFG.num_classes
+        default=CFG["num_classes"]
     )
 
     parser.add_argument(
         "--img_size",
         type=int,
         help="Image size to use",
-        default=CFG.img_size
+        default=CFG["img_size"]
     )
 
     parser.add_argument(
         "--model_path",
         type=str,
         help="Specify a diferent model path from the default 'path/to/hubmap-organ-segmentation/models'",
-        default=CFG.model_path
+        default=CFG["model_path"]
     )
 
     parser.add_argument(
         "--seed",
         type=int,
         help="Seed for random generation",
-        default=CFG.seed
+        default=CFG["seed"]
     )
 
     parser.add_argument(
         "--test_size",
         type=float,
         help="Relative size (0-1) of the test set",
-        default=CFG.test_size
+        default=CFG["test_size"]
     )
 
     parser.add_argument(
         "--n_folds",
         type=int,
         help="Number of folds for cross validation",
-        default=CFG.n_folds
+        default=CFG["n_folds"]
     )
 
     parser.add_argument(
         "--model",
         type=str,
         help="Model to train",
-        default=CFG.model
+        default=CFG["model"]
     )
 
     parser.add_argument(
         "--epochs",
         type=int,
         help="Number of epochs to train",
-        default=CFG.epochs
+        default=CFG["epochs"]
     )
 
     parser.add_argument(
         "--batch_size",
         type=int,
         help="Batch size to train",
-        default=CFG.batch_size
+        default=CFG["batch_size"]
     )
 
     parser.add_argument(
         "--loss",
         type=str,
         help="Loss function to train",
-        default=CFG.loss
+        default=CFG["loss"]
     )
     parser.add_argument(
         "--optimizer",
         type=str,
         help="Optimizer to train",
-        default=CFG.optimizer
+        default=CFG["optimizer"]
     )
     parser.add_argument(
         "--lr",
         type=float,
         help="Initial learning rate",
-        default=CFG.lr
+        default=CFG["lr"]
     )
     parser.add_argument(
         "--weight_decay",
         type=float,
         help="Weight decay for the learning rate",
-        default=CFG.weight_decay
+        default=CFG["weight_decay"]
     )
     parser.add_argument(
         "--scheduler",
         type=str,
         help="Scheduler for the learning rate",
-        default=CFG.scheduler
+        default=CFG["scheduler"]
     )
     parser.add_argument(
         "--device",
         type=str,
         help="Device (CUDA or cpu)",
-        default=CFG.device
+        default=CFG["device"]
     )
     parser.add_argument(
         "--num-workers",
         type=int,
         help="Number of threads use in loading data",
-        default=CFG.num_workers
+        default=CFG["num_workers"]
     )
 
     args = parser.parse_args()
-    for key,value in vars(args).iteritems():
-        setattr(CFG, key, value)
+    for key,value in vars(args).items():
+        CFG[key] = value
 
-    create_datasets(CFG)
-    model_pipeline(CFG)
+    with open(CONFIG_JSON_PATH, 'w') as f:
+        json.dump(CFG, f, indent=1)
+
+    create_datasets()
+    model_pipeline()

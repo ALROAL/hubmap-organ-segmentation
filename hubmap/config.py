dc0552594d12001b65992e53e04f2cefb6939c35
alroa <alroal98@gmail.com>
2022-09-23 16:33:09 +0200

    new default config

--- hubmap/config.py
+++ hubmap/config.py
@@ -8,13 +8,13 @@ CFG = {
     "model_path": "", #specify a diferent model path from the default "path/to/hubmap-organ-segmentation/models"
     "seed": 0,
     "test_size": 0.2,
-    "n_folds": 5,
+    "n_folds": 4,
     "model": "UNet",
-    "epochs": 20,
-    "batch_size": 16,
+    "epochs": 100,
+    "batch_size": 64,
     "loss": "BCE+SoftDice",
     "optimizer": 'Adam',
-    "lr": 1e-4,
+    "lr": 1e-3,
     "weight_decay": 1e-6,
     "scheduler": 'CosineAnnealingLR', #['CosineAnnealingLR', 'ReduceLROnPlateau', 'ExponentialLR']
     "device": "cuda:0" if torch.cuda.is_available() else "cpu",
